6f1930df60b415b29121663429520e92afade98b
AuroraSan <arwa.zakaria@ejust.edu.eg>
2024-03-12 14:55:24 +0200

    new

--- binary_trees.h
+++ binary_trees.h
@@ -6,10 +6,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* Data structures */
 
 /**
  * struct binary_tree_s - Binary tree node
- *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
@@ -17,13 +17,15 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
-
-typedef struct binary_tree_s binary_tree_t; 
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 /**
  * struct levelorder_queue_s - Level order traversal queue.
@@ -35,7 +37,6 @@ typedef struct levelorder_queue_s
 	binary_tree_t *node;
 	struct levelorder_queue_s *next;
 } levelorder_queue_t;
-
 
 /* Printing helper function */
 void binary_tree_print(const binary_tree_t *);
